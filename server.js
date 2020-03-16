8a2beb8f2ca105a11e238425c9be9b7a402f84cd
Thibault BALSAMO <balsamothibault@hotmail.fr>
2020-03-16 15:16:21 +0100

    add /hello

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ app.get('/', function (req, res) {
     res.send("Hello World !")
 })
 
+app.get('/hello', function(req, res) {
+    var nom = req.query.nom
+    if (nom == undefined) {
+        res.send("Quel est votre nom ?")
+    }
+    
+    else {
+        res.send("Bonjour " + nom + " !")
+    }
+})
+
 app.listen(port, function() {
     console.log("Example app listening on port 3000!")
 })
