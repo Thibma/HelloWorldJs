8d39e8ace824a7c1f7af81fd94b2d4ec2476119e
Thibault BALSAMO <balsamothibault@hotmail.fr>
2020-03-18 11:31:20 +0100

    Add dates.js + mongo

---
+++ dates.js
@@ -0,0 +1,32 @@
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+
+async function mongo() {
+    // URL de connexion
+    const url = 'mongodb://localhost:27017'
+
+    // Nom de la base de donnée
+    const dbName = 'nodejs'
+    const client = new MongoClient(url)
+
+    try {
+        // Connexion au serveur
+        await client.connect()
+        const db = client.db(dbName)
+
+        const collection = db.collection('dates')
+
+        let r = await db.collection('dates').insertOne({date: new Date()})
+        assert.equal(1, r.insertedCount)
+    
+        const docs = await collection.find({}).toArray()
+        console.log(docs)
+    }
+    catch (err) {
+        console.log(err.stack)
+    }
+
+    client.close()
+}
+
+mongo()
